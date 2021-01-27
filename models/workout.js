261cdf16cba8b432e1dedb3c543a9c5b9b23862a
Biaka Z <biaka@biakaworks.com>
2021-01-27 11:08:43 -0600

    Finished Workout Model and Run Seed

---
+++ models/workout.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  exercises: [{
+
+    type: {
+      type: String,
+      required: "Enter a workout type"
+    },
+     name: {
+      type: String,
+      required: "Enter a workout name"
+    },
+    duration: {
+      type: Number,
+      required: "Enter a how long the workout take"
+    },
+    distance: {
+      type: Number,
+    },
+    weight: {
+      type: Number,
+    },
+    reps: {
+      type: Number,
+    },
+    sets: {
+      type: Number,
+    },
+  }
+  ],
+  
+  
+  
+ 
+  day: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
